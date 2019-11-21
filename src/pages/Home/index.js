b86e4d9c12f4f8bfc3321bede8515b84e65fd078
gabrielazimpel <gabriela.zimpel@gmail.com>
2019-11-20 21:25:21 -0300

    importando componentes da Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import Menu from '../../components/Menu'
+import Banner from './Banner'
+import Beneficios from './Beneficios'
+import Funcionalidades from './Funcionalidades'
+import Contato from './Contato'
+import Footer from '../../components/Footer'
+
 
 function Home(){
     return(
       <div>
-        <h1>Home</h1>
+        <Menu />
+        <Banner />
+        <Beneficios />
+        <Funcionalidades />
+        <Contato />
+        <Footer />
       </div>
     );
 }
