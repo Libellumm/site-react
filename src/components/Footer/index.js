f507459717b3e265b698a8cc831456de7e6cd809
Catherine <cathy.korres@gmail.com>
2019-11-20 21:51:04 -0300

    inserir conteudo componente footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,9 +2,30 @@ import React from 'react';
 
 function Footer() {
   return (
-    <div>
-      <h1>Footer</h1>
-    </div>
+    <footer>
+      <div className="container">
+        <div>
+          <h1 className="libellum"><img src="/img/icon/dragonfly.svg" alt="Libellum" /> Libellum</h1>
+        </div>
+        
+        <div className="contato">
+          <h6>Telefone: (45)99903 ** **</h6>
+          <h6>E-mail: contato@libellum.com.br</h6>
+          <h6>Endereço: Avenida das Cataratas, 00</h6>
+        </div>
+
+        <div className="redes-sociais">
+          <a href=""><img src="/img/icon/social/facebook-f-brands.svg" alt="" width="30" height="30" /></a>
+          <a href=""><img src="/img/icon/social/linkedin-in-brands.svg" alt="" width="30" height="30" /></a>
+          <a href=""><img src="/img/icon/social/twitter-brands.svg" alt="" width="30" height="30" /></a>
+          <a href=""><img src="/img/icon/social/whatsapp-brands.svg" alt="" width="30" height="30" /></a>
+        </div>
+      </div>
+      
+      <div className="copyright">
+        <p>Todos os direitos reservados © 2019 Libellum</p>
+      </div>
+    </footer>
   );
 }
 
