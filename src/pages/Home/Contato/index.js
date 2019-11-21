ec306908f0bfdf6bca582f3b8d7a7def1a2f247c
gabrielazimpel <gabriela.zimpel@gmail.com>
2019-11-20 21:49:32 -0300

    Inserir componente Contato

--- src/pages/Home/Contato/index.js
+++ src/pages/Home/Contato/index.js
@@ -4,7 +4,16 @@ import React from 'react';
 function Contato() {
     return (
         <div>
-            <h1>hello world contato </h1>
+            <section id="formulario">
+                    <div class="content">
+                        <h2 class="section-title">Ficou interessado?</h2>
+                        <h4>Realize sua pré-inscrição para acompanhar o nosso lançamento.</h4>
+                        <form action="">
+                            <input type="email" name="email" placeholder="Digite seu melhor e-mail"/>
+                            <input type="submit" value="Inscreva-se"/>
+                        </form> 
+                    </div>
+            </section>
         </div>
     );
 }
