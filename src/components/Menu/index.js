f2cb2a09bce3836bcd90cc564b1222d4e755e050
Catherine <cathy.korres@gmail.com>
2019-11-20 22:15:01 -0300

    adicionando estilos do menu

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import './menu.css';
+
+import IconLogo from '../../assets/icon/dragonfly.svg';
 
 function Menu() {
   return(
     <header id="menu">
-      <a href="#" className="libellum h4"><img src="/img/icon/dragonfly.svg" alt="Libellum" />Libellum</a>
+      <a href="#" className="libellum h4"><img src={IconLogo} alt="Libellum" />Libellum</a>
 
       <nav>
         <a href="/dashboard" id="login">Login</a>
