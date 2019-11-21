9d7ebf8f906f9774372669ee49ea266288a06724
Ana Santoro <annanicollau@gmail.com>
2019-11-20 21:47:41 -0300

    Inserir componente funcionalidades

--- src/pages/Home/Funcionalidades/index.js
+++ src/pages/Home/Funcionalidades/index.js
@@ -3,7 +3,42 @@ import React from 'react';
 function Funcionalidades() {
     return (
         <div>
-            <h1>Funcionalidades</h1>
+            <section id="funcionalidades">
+        <div className="content">
+          <h2 className="section-title">Funcionalidades</h2>
+
+          <div className="container">
+            <div className="item">
+              <div className="img" id="cadastro-de-eventos"></div>
+
+              <div className="texto">
+                <h4>Cadastro de Eventos</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam et metus sodales, ultrices dui et, blandit enim. Aliquam in aliquet nunc. Phasellus sagittis sit amet justo id aliquet. Donec eu augue quis tellus elementum imperdiet. Duis vel gravida magna. Nunc quis lobortis dolor. Suspendisse porttitor, orci id tincidunt consequat, nisi ante hendrerit erat, vel varius nunc mi in turpis.</p>
+              </div>
+            </div>
+
+            <div className="item">
+              <div className="img" id="marcacao-de-presenca"></div>
+
+              <div className="texto">
+                <h4>Marcação on-line de presença</h4>
+      
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam et metus sodales, ultrices dui et, blandit enim. Aliquam in aliquet nunc. Phasellus sagittis sit amet justo id aliquet. Donec eu augue quis tellus elementum imperdiet. Duis vel gravida magna. Nunc quis lobortis dolor. Suspendisse porttitor, orci id tincidunt consequat, nisi ante hendrerit erat, vel varius nunc mi in turpis.</p>
+              </div>       
+            </div>
+
+            <div className="item">
+              <div className="img" id="envio-de-certificado"></div>
+
+              <div className="texto">
+                <h4>Envio de certificado digital</h4>
+      
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam et metus sodales, ultrices dui et, blandit enim. Aliquam in aliquet nunc. Phasellus sagittis sit amet justo id aliquet. Donec eu augue quis tellus elementum imperdiet. Duis vel gravida magna. Nunc quis lobortis dolor. Suspendisse porttitor, orci id tincidunt consequat, nisi ante hendrerit erat, vel varius nunc mi in turpis.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
         </div>
     );
 }
