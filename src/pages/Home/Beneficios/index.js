90c5c6fc2175631035cba7bea950999715a0e5c4
Ana Santoro <annanicollau@gmail.com>
2019-11-20 21:36:54 -0300

    Componentes

---
+++ src/pages/Home/Beneficios/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Beneficios() {
+    return (
+        <div>
+            <h1>Benefícios</h1>
+        </div>
+    );
+}
+
+export default Beneficios;
+
---
+++ src/pages/Home/Funcionalidades/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Funcionalidades() {
+    return (
+        <div>
+            <h1>Funcionalidades</h1>
+        </div>
+    );
+}
+
+export default Funcionalidades;
