0752040677da3e75f41758b43176bc12a516fea0
Ana Santoro <annanicollau@gmail.com>
2019-11-20 21:47:30 -0300

    Inserir componente benfícios

--- src/pages/Home/Beneficios/index.js
+++ src/pages/Home/Beneficios/index.js
@@ -3,7 +3,37 @@ import React from 'react';
 function Beneficios() {
     return (
         <div>
-            <h1>Benefícios</h1>
+             <section id="beneficios">
+        <div className="content">
+          <h2 className="section-title">Benefícios</h2>
+
+          <div className="container">
+            <div className="item">
+              <div className="img">
+                <img src="/img/icon/home/computer.svg" width="100" height="100" alt="Automatização" />
+              </div>
+              <h4>Automatização</h4>
+              <p>Envie certificados de participação do seu evento automaticamente.</p>
+            </div>
+
+            <div className="item">
+              <div className="img">
+                <img src="/img/icon/home/clock.svg" width="100" height="100" alt="Agilidade" />
+              </div>
+              <h4>Agilidade</h4>
+              <p>Gere listas de participantes que compareceram no seu evento de forma fácil e rápida.</p>
+            </div>
+
+            <div className="item">
+              <div className="img">
+                <img src="/img/icon/home/customize.svg" width="100" height="100" alt="Customização" />
+              </div>
+              <h4>Customização</h4>
+              <p>Personalize o seu certificado de acordo com seu evento</p>
+            </div>
+          </div>
+        </div>
+      </section>
         </div>
     );
 }
