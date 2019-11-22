92facf8385bcb715a834ec46419b88c460c74ea8
gabrielazimpel <gabriela.zimpel@gmail.com>
2019-11-21 21:02:04 -0300

    Inclusão do componente Statistic

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Sidebar from './Sidebar';
+import Statistic from './Statistic';
 
 function Home(){
     return(
       <div>
           <Sidebar />
+          <Statistic />
       </div>
     );
 }
