f7348da8b82984b87a397d17361481236a91173c
Ana Santoro <annanicollau@gmail.com>
2019-12-09 18:58:10 -0300

    adicionado rotas para login e cadastro

--- src/App.js
+++ src/App.js
@@ -6,12 +6,17 @@ import Home from './pages/Home';
 import Dashboard from './pages/Dashboard';
 import Calendario from './pages/Calendario';
 import Formulário from './pages/Criar-Evento';
+import Login from './pages/Login'
+import Singin from './pages/Singin'
+
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login}/>
+        <Route exact path="/singin" component={Singin}/>
         <Route path="/dashboard" component={Dashboard} />
         <Route path="/eventos" component={Calendario} />
         <Route path="/criar-evento" component={Formulário} />
