c06a80588632371b6844bfc271b344aa4ef9c1ec
gabrielazimpel <gabriela.zimpel@gmail.com>
2019-12-11 19:51:32 -0300

    Correção de sinais nos imports

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Menu from '../../components/Menu'
-import Banner from './Banner'
-import Beneficios from './Beneficios'
-import Funcionalidades from './Funcionalidades'
-import Contato from './Contato'
-import Footer from '../../components/Footer'
+import Menu from '../../components/Menu';
+import Banner from './Banner';
+import Beneficios from './Beneficios';
+import Funcionalidades from './Funcionalidades';
+import Contato from './Contato';
+import Footer from '../../components/Footer';
 
 
 function Home(){
